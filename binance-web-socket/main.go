b17e4547e2449a6552eeb83e44239ef2d738ac07
Weltburger <pakowski398@yandex.ru>
2021-09-22 10:02:58 +0300

    main ws + handler done

--- binance-web-socket/main.go
+++ binance-web-socket/main.go
@@ -3,52 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
-	"log"
 	"net/http"
+	//"time"
 )
 
 var upgrader = websocket.Upgrader{}
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		mesType, mes, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
+var messageSent = []byte("Pong")
+func handler(writer http.ResponseWriter, request *http.Request) {
+	var conn, _ = upgrader.Upgrade(writer, request, nil)
+	go func(conn *websocket.Conn) {
+		for {
+			_, messageRecieved, _ := conn.ReadMessage()
+			fmt.Println(string(messageRecieved))
+			conn.WriteMessage(1, messageSent)
+			fmt.Println(string(messageSent))
 		}
-
-		log.Println(mes)
-
-		if err := conn.WriteMessage(mesType, mes); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true	}
-
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Println("Successfully connected")
-
-	reader(ws)
-}
-
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
+	}(conn)
 }
 
 func main() {
-	setupRoutes()
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	http.HandleFunc("/ws", handler)
+
+	http.ListenAndServe(":3000", nil)
 }
