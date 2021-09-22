241c57974f5061a23ca212f82f8c49bf68cf6a70
Weltburger <pakowski398@yandex.ru>
2021-09-22 11:25:56 +0300

    main ws functionality done

--- binance-web-socket/main.go
+++ binance-web-socket/main.go
@@ -1,29 +1,113 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"log"
 	"net/http"
-	//"time"
+	"os"
+	"strings"
 )
+
+type Trade struct {
+	E  string `json:"e"`
+	E1 int    `json:"E"`
+	S  string `json:"s"`
+	A  int    `json:"a"`
+	P  string `json:"p"`
+	Q  string `json:"q"`
+	F  int    `json:"f"`
+	L  int    `json:"l"`
+	T  int    `json:"T"`
+	M  bool   `json:"m"`
+}
+
+type Request struct {
+	Method string    `json:"method"`
+	Params [1]string `json:"params"`
+	ID     int       `json:"id"`
+}
 
 var upgrader = websocket.Upgrader{}
 
-var messageSent = []byte("Pong")
 func handler(writer http.ResponseWriter, request *http.Request) {
-	var conn, _ = upgrader.Upgrade(writer, request, nil)
-	go func(conn *websocket.Conn) {
+	var conn, err = upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ws, _, err := websocket.DefaultDialer.Dial("wss://stream.binance.com:9443/ws", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := new(Trade)
+	go func(ws *websocket.Conn) {
 		for {
-			_, messageRecieved, _ := conn.ReadMessage()
-			fmt.Println(string(messageRecieved))
-			conn.WriteMessage(1, messageSent)
-			fmt.Println(string(messageSent))
+			_, message, readErr := ws.ReadMessage()
+			if readErr != nil {
+				fmt.Println(readErr)
+				return
+			}
+
+			if err := json.Unmarshal(message, &data); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(data)
 		}
-	}(conn)
+	}(ws)
+	
+	go func(conn *websocket.Conn, ws *websocket.Conn) {
+		for {
+			_, mes, err := conn.ReadMessage()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if strings.ToLower(string(mes)) == "stop" {
+				os.Exit(1)
+			}
+
+			params := strings.Split(string(mes), " ")
+			if len(params) > 1 {
+				if params[0] == "SUBSCRIBE" {
+					newRequest := Request{params[0], [1]string{params[1]+"@trade"}, 1 }
+					err = ws.WriteJSON(newRequest)
+					if err != nil {
+						log.Fatal(err)
+					}
+				} else if params[0] == "UNSUBSCRIBE"{
+					newRequest := Request{params[0], [1]string{params[1]+"@trade"}, 312}
+					err = ws.WriteJSON(newRequest)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+
+			}
+
+		}
+	}(conn, ws)
+}
+
+func stopper(writer http.ResponseWriter, request *http.Request) {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Stopping the application", string(body))
+	_, err = writer.Write([]byte("Stopping the application"))
+	if err != nil {
+		return
+	}
+
+	os.Exit(1)
 }
 
 func main() {
 	http.HandleFunc("/ws", handler)
+	http.HandleFunc("/stop", stopper)
 
 	http.ListenAndServe(":3000", nil)
 }
