62e42c4009933094a63c8b9158dbc05154eec399
Weltburger <pakowski398@yandex.ru>
2021-10-08 11:25:45 +0300

    queries added

---
+++ binance-web-socket/Queries.sql
@@ -0,0 +1,78 @@
+CREATE TABLE IF NOT EXISTS trade.trades (
+    uuid         UUID,
+    event_type   String,
+    event_time   DateTime,
+    symbol       String,
+    trade_id      UInt64,
+    price        String,
+    quantity     String,
+    first_trade_id UInt64,
+    last_trade_id  UInt64,
+    trade_time    DateTime,
+    market_maker  UInt8
+) engine = MergeTree()
+PARTITION BY toYYYYMMDD(event_time)
+order by  (event_type, symbol)
+
+-------------------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS trade.trades_rep (
+    uuid         UUID,
+    event_type   String,
+    event_time   DateTime,
+    symbol       String,
+    trade_id      UInt64,
+    price        String,
+    quantity     String,
+    first_trade_id UInt64,
+    last_trade_id  UInt64,
+    trade_time    DateTime,
+    market_maker  UInt8
+) engine = ReplacingMergeTree()
+PARTITION BY toYYYYMMDD(event_time)
+order by  (trade_id)
+TTL event_time + INTERVAL 1 WEEK
+SETTINGS index_granularity=262144,
+         index_granularity_bytes=0
+
+-------------------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS trade.trades_sum (
+    uuid         UUID,
+    event_type   String,
+    event_time   DateTime,
+    symbol       String,
+    trade_id      UInt64,
+    price        String,
+    quantity     String,
+    first_trade_id UInt64,
+    last_trade_id  UInt64,
+    trade_time    DateTime,
+    market_maker  UInt8
+) engine = SummingMergeTree()
+      PARTITION BY toYYYYMMDD(event_time)
+      order by  (trade_id)
+      TTL event_time + INTERVAL 1 WEEK
+      SETTINGS index_granularity=262144,
+          index_granularity_bytes=0
+
+-------------------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS trade.trades_agg (
+    uuid         UUID,
+    event_type   String,
+    event_time   DateTime,
+    symbol       String,
+    trade_id      UInt64,
+    price        String,
+    quantity     String,
+    first_trade_id UInt64,
+    last_trade_id  UInt64,
+    trade_time    DateTime,
+    market_maker  UInt8
+) engine = AggregatingMergeTree()
+      PARTITION BY toYYYYMMDD(event_time)
+      order by  (trade_id)
+      TTL event_time + INTERVAL 1 WEEK
+      SETTINGS index_granularity=262144,
+          index_granularity_bytes=0
